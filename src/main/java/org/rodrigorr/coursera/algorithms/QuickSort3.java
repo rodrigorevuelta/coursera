70ed7afd114becff5881b7de51f8c328f7657fe0
rodrigorevueltaroca@gmail.com <rodrigorevueltaroca@gmail.com>
2014-03-28 23:15:54 +0100

    añadido quicksort3

---
+++ src/main/java/org/rodrigorr/coursera/algorithms/QuickSort3.java
@@ -0,0 +1,41 @@
+package org.rodrigorr.coursera.algorithms;
+
+import org.rodrigorr.coursera.util.StdRandom;
+
+public class QuickSort3 {
+
+	public static void sort(Comparable[] a) {
+		StdRandom.shuffle(a);
+		sort(a, 0, a.length - 1);
+	}
+
+	private static void sort(Comparable[] a, int lo, int hi) {
+		if (hi <= lo)
+			return;
+		int lt = lo, gt = hi;
+		Comparable v = a[lo];
+		int i = lo;
+		while (i <= gt) {
+			int cmp = a[i].compareTo(v);
+			if (cmp < 0)
+				exch(a, lt++, i++);
+			else if (cmp > 0)
+				exch(a, i, gt--);
+			else
+				i++;
+		}
+		sort(a, lo, lt - 1);
+		sort(a, gt + 1, hi);
+	}
+
+	private static boolean less(final Comparable v, final Comparable w) {
+		return v.compareTo(w) < 0;
+	}
+
+	private static void exch(Object[] a, int i, int j) {
+		Object swap = a[i];
+		a[i] = a[j];
+		a[j] = swap;
+	}
+
+}
--- src/main/java/org/rodrigorr/coursera/main/Main.java
+++ src/main/java/org/rodrigorr/coursera/main/Main.java
@@ -1,23 +1,30 @@
 package org.rodrigorr.coursera.main;
 
 import org.rodrigorr.coursera.algorithms.MergeSortBU;
+import org.rodrigorr.coursera.algorithms.QuickSort;
+import org.rodrigorr.coursera.algorithms.QuickSort3;
 import org.rodrigorr.coursera.util.Util;
 
 public class Main {
 
-    /**
-     * @param args
-     */
-    public static void main(final String[] args) {
+	/**
+	 * @param args
+	 */
+	public static void main(final String[] args) {
 
-        // final Integer[] elements = new Integer[] { 58, 93, 25, 78, 75, 52, 73, 68, 98, 36, 79, 45 };
-        final Integer[] elements = new Integer[] { 65, 55, 70, 68, 43, 74, 47, 88, 40, 75 };
+		// final Integer[] elements = new Integer[] { 58, 93, 25, 78, 75, 52,
+		// 73, 68, 98, 36, 79, 45 };
+		// final Integer[] elements = new Integer[] { 65, 55, 70, 68, 43, 74, 47, 88, 40, 75 };
+		// String valuesStr = new String("52 13 86 68 49 33 57 23 39 26 69 47");
+		String valuesStr = new String("54 54 80 68 74 54 76 64 84 39");
+		String[] values = valuesStr.split(" ");
+		// MergeSort.sort(elements);
+		// MergeSortBU.sort(elements);
+		// QuickSort.sort(values);
+		QuickSort3.sort(values);
+		// Util.imprimirArray(0, elements);
+		Util.imprimirArray(0, values);
 
-        // MergeSort.sort(elements);
-        MergeSortBU.sort(elements);
-
-        Util.imprimirArray(0, elements);
-
-    }
+	}
 
 }
