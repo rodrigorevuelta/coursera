1a6a7f1a3ae9a8f35904177b3d90cd6c428d4f28
rodrigorr <rodrigorevueltaroca@gmail.com>
2014-04-05 22:40:22 +0200

    bst

--- src/main/java/org/rodrigorr/coursera/main/Main.java
+++ src/main/java/org/rodrigorr/coursera/main/Main.java
@@ -1,5 +1,9 @@
 package org.rodrigorr.coursera.main;
 
+import java.util.Iterator;
+
+import org.rodrigorr.coursera.algorithms.BST;
+import org.rodrigorr.coursera.algorithms.MaxPQ;
 import org.rodrigorr.coursera.algorithms.QuickSort;
 
 public class Main {
@@ -13,16 +17,33 @@ public class Main {
 		// 73, 68, 98, 36, 79, 45 };
 		// final Integer[] elements = new Integer[] { 65, 55, 70, 68, 43, 74,
 		// 47, 88, 40, 75 };
-		String valuesStr = new String("52 13 86 68 49 33 57 23 39 26 69 47");
-//		String valuesStr = new String("54 54 80 68 74 54 76 64 84 39");
+
+		// String valuesStr = new String("54 54 80 68 74 54 76 64 84 39");
+		String valuesStr = new String("86 11 94 36 98 34 46 16 60 12 61 78");
 		String[] values = valuesStr.split(" ");
+
+		BST<String, String> bst = new BST<String, String>();
+		for (String value : values) {
+			bst.put(value, value);
+		}
+		bst.delete("12");
+		bst.delete("46");
+		bst.delete("36");
+		Iterable<String> ts = bst.keys();
+		Iterator it = ts.iterator();
+		while (it.hasNext()){
+			System.out.print(" "+it.next());
+		}
+
+	}
+
+	private void sort() {
 		// MergeSort.sort(elements);
 		// MergeSortBU.sort(elements);
-		QuickSort.sort(values);
-//		 QuickSort3.sort(values);
+		// QuickSort.sort(values);
+		// QuickSort3.sort(values);
 		// Util.imprimirArray(0, elements);
 		// Util.imprimirArray(0,0, values);
-
 	}
 
 }
--- src/main/java/org/rodrigorr/coursera/util/Util.java
+++ src/main/java/org/rodrigorr/coursera/util/Util.java
@@ -2,11 +2,22 @@ package org.rodrigorr.coursera.util;
 
 public final class Util {
 
-    public static void imprimirArray(final int i, final int j,final Comparable[] array) {
-        System.out.print("values : "+ i + " "+ j +"[");
-        for (final Comparable c : array) {
-            System.out.print(c.toString() + " ");
-        }
-        System.out.println("]");
-    }  
+	public static void imprimirArray(final Comparable[] array) {
+		System.out.print("values : [");
+		for (final Comparable c : array) {
+			if (c != null) {
+				System.out.print(c.toString() + " ");
+			}
+		}
+		System.out.println("]");
+	}
+
+	public static void imprimirArray(final int i, final int j,
+			final Comparable[] array) {
+		System.out.print("values : " + i + " " + j + "[");
+		for (final Comparable c : array) {
+			System.out.print(c.toString() + " ");
+		}
+		System.out.println("]");
+	}
 }
