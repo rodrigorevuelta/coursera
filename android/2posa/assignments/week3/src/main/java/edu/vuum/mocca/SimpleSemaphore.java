08febfb72c17afb5e9b64eac489d2d55df61ce04
rodrigorevuelta <rodrigorevueltaroca@gmail.com>
2014-05-28 10:31:40 +0200

    Init week 3

--- android/2posa/assignments/week3/src/main/java/edu/vuum/mocca/SimpleSemaphore.java
+++ android/2posa/assignments/week3/src/main/java/edu/vuum/mocca/SimpleSemaphore.java
@@ -1,51 +1,52 @@
 package edu.vuum.mocca;
 
-import java.util.concurrent.locks.ReentrantLock;
-import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * @class SimpleSemaphore
  * 
- * @brief This class provides a simple counting semaphore implementation using
- *        Java a ReentrantLock and a ConditionObject. It must implement both
- *        "Fair" and "NonFair" semaphore semantics, just liked Java Semaphores.
+ * @brief This class provides a simple counting semaphore implementation using Java a ReentrantLock and a
+ *        ConditionObject. It must implement both "Fair" and "NonFair" semaphore semantics, just liked Java Semaphores.
  */
 public class SimpleSemaphore {
     /**
      * Define a ReentrantLock to protect the critical section.
      */
     // TODO - you fill in here
+    ReentrantLock rl = new ReentrantLock();
 
     /**
-     * Define a ConditionObject to wait while the number of
-     * permits is 0.
+     * Define a ConditionObject to wait while the number of permits is 0.
      */
     // TODO - you fill in here
+    Condition co;
 
     /**
      * Define a count of the number of available permits.
      */
-    // TODO - you fill in here.  Make sure that this data member will
-    // ensure its values aren't cached by multiple Threads..
+    // TODO - you fill in here. Make sure that this data member will
+    // ensure its values aren't cached by multiple Threads.
+    private final SimpleAtomicLong sal;
 
-    public SimpleSemaphore(int permits, boolean fair) {
+    public SimpleSemaphore(final int permits, final boolean fair) {
         // TODO - you fill in here to initialize the SimpleSemaphore,
         // making sure to allow both fair and non-fair Semaphore
         // semantics.
+        this.rl = new ReentrantLock(fair);
+        this.sal = new SimpleAtomicLong(permits);
     }
 
     /**
-     * Acquire one permit from the semaphore in a manner that can be
-     * interrupted.
+     * Acquire one permit from the semaphore in a manner that can be interrupted.
      */
     public void acquire() throws InterruptedException {
         // TODO - you fill in here.
+        this.rl.lockInterruptibly();
     }
 
     /**
-     * Acquire one permit from the semaphore in a manner that cannot be
-     * interrupted.
+     * Acquire one permit from the semaphore in a manner that cannot be interrupted.
      */
     public void acquireUninterruptibly() {
         // TODO - you fill in here.
@@ -64,6 +65,6 @@ public class SimpleSemaphore {
     public int availablePermits() {
         // TODO - you fill in here by changing null to the appropriate
         // return value.
-        return null;
+        return (int) this.sal.get();
     }
 }
